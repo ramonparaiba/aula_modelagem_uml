7626a43163a3656b66a5c7fa3ac1385d3e7c2cb4
ramonparaiba <ramonparaiba@gmail.com>
2018-04-16 22:59:58 -0300

    Primeiro Commit

---
+++ src/main/java/com/pabloramon/aulauml/AulaModelagemUmlApplication.java
@@ -0,0 +1,12 @@
+package com.pabloramon.aulauml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaModelagemUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaModelagemUmlApplication.class, args);
+	}
+}
